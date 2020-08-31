9c08bbed27df0c57c5a2f6c677b6895b2df1574e
Wahyu Adhe Triwibowo <adhedewa1996@gmail.com>
2020-08-31 21:46:03 +0700

    Add Bottom Cart

--- src/component/Calender/Calender.js
+++ src/component/Calender/Calender.js
@@ -4,38 +4,64 @@ import './Calender.css';
 
 function Calender(){
     return(
-        <Swiper spaceBetween={50} slidesPerView={3}>
+        <Swiper  >
             <SwiperSlide className="calender">
-                {/* <div className="calender"> */}
-                    <div className="day">
-                        SEN
-                        <div className="date">10</div>
-                    </div>
-                    <div className="day">
-                        SEL
-                        <div className="date">11</div>
-                    </div>
-                    <div className="day">
-                        RAB
-                        <div className="date">12</div>
-                    </div>
-                    <div className="day active">
-                        KAM
-                        <div className="date">13</div>
-                    </div>
-                    <div className="day">
-                        JUM
-                        <div className="date">14</div>
-                    </div>
-                    <div className="day disable">
-                        SAB
-                        <div className="date">15</div>
-                    </div>
-                    <div className="day disable">
-                        MIN
-                        <div className="date">16</div>
-                    </div>
-                {/* </div> */}
+                <div className="day">
+                    SEN
+                    <div className="date">10</div>
+                </div>
+                <div className="day">
+                    SEL
+                    <div className="date">11</div>
+                </div>
+                <div className="day">
+                    RAB
+                    <div className="date">12</div>
+                </div>
+                <div className="day active">
+                    KAM
+                    <div className="date">13</div>
+                </div>
+                <div className="day">
+                    JUM
+                    <div className="date">14</div>
+                </div>
+                <div className="day disable">
+                    SAB
+                    <div className="date">15</div>
+                </div>
+                <div className="day disable">
+                    MIN
+                    <div className="date">16</div>
+                </div>
+                <div className="day">
+                    SEN
+                    <div className="date">17</div>
+                </div>
+                <div className="day">
+                    SEL
+                    <div className="date">18</div>
+                </div>
+                <div className="day">
+                    RAB
+                    <div className="date">19</div>
+                </div>
+                <div className="day">
+                    KAM
+                    <div className="date">20</div>
+                </div>
+                <div className="day">
+                    JUM
+                    <div className="date">21</div>
+                </div>
+                <div className="day disable">
+                    SAB
+                    <div className="date">22</div>
+                </div>
+                <div className="day disable">
+                    MIN
+                    <div className="date">23</div>
+                </div>
             </SwiperSlide>
         </Swiper>
     );
--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './Card.css';
 import StarIcon from '@material-ui/icons/Star';
 import StarHalfIcon from '@material-ui/icons/StarHalf';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 function Card() {
+    let x = 0;
+
+    function addItem(e) {
+        x = x +1;
+        alert(x);
+    }
+
     return(
-        <div className="card">
+        <Fragment>
+            <div className="card">
             <img src="https://lh3.googleusercontent.com/proxy/6sK68pvAyWDiJNJhMNAx0gpytfe7sVBC5wyW97T566S5a7zVpBdFXG_PES8h-ElRdExWZnSsYarwx_Q5u-QnYE-gpBLbetlVfGGJdjjVuagpfyjcn9S9BqI7gbM3fERqBYpEGA" alt=""/>
             <div className="info-card">
                 <div className="rating">
@@ -21,9 +31,21 @@ function Card() {
             </div>
             <div className="info-order">
                 <div className="food-price" >Rp 35.000 </div>
-                <div className="add"> ADD + </div>
+                <button className="add" onClick={addItem}> ADD + </button>
             </div>
         </div>
+        <div className="blur"></div>
+        <div className="cart">
+            <div className="info-cart">
+                <div className="atas">
+                    <div className="jumlah"> 5 Items | </div>
+                    <div className="total"> Rp 125.000 </div>
+                </div>
+                <div className="bawah"> Termasuk ongkos kirim </div>
+            </div>
+            <div className="icon"> <ShoppingCartIcon /> <ArrowForwardIosIcon/> </div>
+        </div>
+        </Fragment>
     );
 }
 
